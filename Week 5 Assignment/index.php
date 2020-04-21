2f5404d70093a982632f40c68b27a626b75d6ce9
SathwikSharma226 <53095162+SathwikSharma226@users.noreply.github.com>
2020-04-21 17:55:37 +0530

    Create index.php

---
+++ Week 5 Assignment/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Sathwik N Sharma PHP</title>
+	</head>
+	<body>
+		<h1>Sathwik N Sharma PHP</h1>
+
+		<pre>ASCII ART (First letter of the Name 'Sathwik' - 'S'):
+
+    **********
+    **********
+    **
+    **
+    **********
+    **********
+            **
+            **
+    **********
+    **********        
+		</pre>
+
+		<p>
+			The SHA256 hash of "Sathwik N Sharma" is <?php print hash('sha256', 'Sathwik N Sharma'); ?>
+		</p>
+	
+	</body>
+</html>
